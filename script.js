7685ddceafa1dd0c55265815f141688a998cf71f
SimonCura <scqdevelopers@gmail.com>
2020-11-18 10:13:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
 let user = {
-    name: "Caden",
-    age: 2,
-    hobby: "Serratoness",
-    isMarried: false,
-    spells: ["dafoufive", "fiddycent"],
-    shout: function() {
-        console.log("AAAHHHH");
-    }
-};
-
-let userArray = ["five", "six", "eight", {username: "password"}, ];
-
-console.log(userArray[3].username);
+	username: "Simon",
+	password: "supercoolness",
+}
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+	{
+		username: "Simon",
+		password: "supercoolness"
+	}
+];
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "guy769123",
+		timeline: "goodbye world? is the opposite of hello world hehe"
+	},
+	{
+		username: "nebula12314",
+		timeline: "I love minecraft"
+	},
+	{
+		username: "Mitch",
+		timeline: "You Son of MITCH!"
+	}
+];
