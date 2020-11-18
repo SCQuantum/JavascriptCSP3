5add300cf9f48365b8787917a1ed46613235157e
SimonCura <scqdevelopers@gmail.com>
2020-11-18 09:57:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// using this array,
-// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
-var array = ["Banana", "Apples", "Oranges", "Blueberries"];
-// 1. Remove the Banana from the array.
-var array = ["Apples", "Oranges", "Blueberries"];
-// 2. Sort the array in order.
-var array = ["Apples", "Blueberries", "Oranges"];
-// 3. Put "Kiwi" at the end of the array.
-var array = ["Apples", "Blueberries", "Oranges", "Kiwi"];
-// 4. Remove "Apples" from the array.
-var array = ["Blueberries", "Oranges", "Kiwi"];
-// 5. Sort the array in reverse order. (Not alphabetical, but reverse
-// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
-var array = ["Kiwi", "Oranges", "Blueberries"];
-//you should have at the end:
-//["Kiwi", "Oranges", "Blueberries"]
+let user = {
+    name: "Caden",
+    age: 2,
+    hobby: "Serratoness",
+    isMarried: false,
+    spells: ["dafoufive", "fiddycent"],
+    shout: function() {
+        console.log("AAAHHHH");
+    }
+};
 
-// using this array,
-// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-// access "Oranges".
-var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-array2[1][1];
+let userArray = ["five", "six", "eight", {username: "password"}, ];
+
+console.log(userArray[3].username);
