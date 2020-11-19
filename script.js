ed002fb06ba998d647b2597645e71a3e123b2596
SimonCura <scqdevelopers@gmail.com>
2020-11-18 23:05:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-{
-	username: "andrei",
-	password: "supersecret"
+//function
+function newFunctionTest() {
+
 }
-var database = [
-	{
-		username: "andrei",
-		password: "supersecret"
+
+//function expression
+var newFunctionExpression = function() {
+
+};
+
+//expression
+1+3;
+var a = 2;
+return true;
+
+//calling or invoking a function
+alert();
+newFunctionTest(param1, param2);
+
+//assign a variable
+var a = true;
+
+//function vs method
+function thisIsAFunction() {
+
+}
+
+var obj = {
+	thisIsAMethod: function() {
+
 	}
-];
-var newsfeed = [
-	{
-		username: "Bobby",
-		timeline: "So tired from all that learning!"
-	},
-	{
-		username: "Sally",
-		timeline: "Javascript is sooooo cool!"
-	},
-	{
-		username: "Mitch",
-		timeline: "Javascript is preeetyy cool!"
-	}
-];
+}
+
+thisIsAFunction()
+obj.thisIsAMethod()
